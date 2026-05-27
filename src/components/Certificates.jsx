f81568676f12978e5b4b88a029844c0fd4c2862e
Jonathan Wu <Jtzw21@gmail.com>
2026-05-26 21:25:44 -0400

    Create Certificates.jsx

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,24 @@
+import react from 'react';
+
+function Certificates() {
+    return (
+        <div className="certificates">
+            <p className="section-kicker">certificates</p>
+            <h2>Certifications</h2>
+            <ul className="certificate-list">
+                <li>
+                    <h3>Certificate Name</h3>
+                    <p>Issuing Organization - Date</p>
+                </li>
+                <li>
+                    <h3>Certificate Name</h3>
+                    <p>Issuing Organization - Date</p>
+                </li>
+                <li>
+                    <h3>Certificate Name</h3>
+                    <p>Issuing Organization - Date</p>
+                </li>
+            </ul>
+        </div>
+    );
+}
